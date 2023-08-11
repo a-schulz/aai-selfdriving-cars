400971e7c1f923594f604c449fa07f1ab3835dec
jay-hae <jakobhaecker@web.de>
2023-08-11 18:45:57 +0200

    data prepararation for traffic light ready

--- src/custom_utils.py
+++ src/custom_utils.py
@@ -1,13 +1,15 @@
 import os
 import cv2
 
+
 def is_image(filename):
     # check if file is an image
-    image_extensions = ['.jpg', '.jpeg', '.png']  
+    image_extensions = ['.jpg', '.jpeg', '.png']
     _, file_extension = os.path.splitext(filename)
     return file_extension.lower() in image_extensions
 
-def get_images(dir, count = -1):
+
+def get_images(dir, count=-1):
     # load images which have to be evaluated from the ai into a list
     # images are in original size and color
     # if bound is positive: only first count files (not images) get analyzed
@@ -28,6 +30,7 @@ def get_images(dir, count = -1):
 
     return paths, images
 
+
 def process_txt_for_label(directory_path, label):
     data_dict = {}
     for filename in os.listdir(directory_path):
@@ -40,10 +43,12 @@ def process_txt_for_label(directory_path, label):
                     values = line.split()
                     integer_value = int(values[0])
                     if integer_value == label:
-                        data_list.append([float(value) for value in values[1:]])
+                        data_list.append([float(value)
+                                         for value in values[1:]])
                 if data_list:
                     data_dict[filename[:-4]] = [label] + data_list
     return data_dict
+
 
 def extract_rectangle_from_image(image, points):
     """
@@ -61,10 +66,11 @@ def extract_rectangle_from_image(image, points):
     y2_pixel = int(y2 * height)
 
     extracted_rect = image[y1_pixel:y2_pixel, x1_pixel:x2_pixel]
-    
+
     return extracted_rect
 
-def write_images(images, path, main_name = "", type = ".jpg"):
+
+def write_images(images, path, main_name="", type=".jpg"):
     # Todo
     """
     if len(images) == 1:
@@ -73,13 +79,15 @@ def write_images(images, path, main_name = "", type = ".jpg"):
         cv2.imwrite(os.path.join(path, main_name), images)
     """
     for idx, i in enumerate(images):
-        cv2.imwrite(os.path.join(path, (main_name + str(idx) + type)), i)
+        cv2.imwrite(os.path.join(path, (main_name + str(idx) + type)),
+                    cv2.cvtColor(i, cv2.COLOR_BGR2RGB))
+
 
 def convert_dataset(categories, src_dir, dest_dir):
     # convert yolo v8 dataset format to which we used in lectures for image classification
     # the  order of categories have to be the same as in the yaml file
     for data_use in ["train", "valid", "test"]:
-        
+
         # create folder for each category in dest_dir
         for c in categories:
             path = dest_dir + "/" + c
@@ -88,7 +96,7 @@ def convert_dataset(categories, src_dir, dest_dir):
 
         # path where the txt files get stored to not destroy the box position
         dest_path_txt = dest_dir + "/__labels__"
-        if not os.path.exists(dest_path_txt ):
+        if not os.path.exists(dest_path_txt):
             os.mkdir(dest_path_txt)
 
         txt_path = os.path.join(src_dir, data_use, "labels")
@@ -97,36 +105,37 @@ def convert_dataset(categories, src_dir, dest_dir):
                 txt_filepath = os.path.join(txt_path, filename_txt)
                 with open(txt_filepath, 'r') as file:
                     lines = file.readlines()
-                    label = int(lines[0].split()[0])  # Label aus der ersten Zeile extrahieren
+                    # Label aus der ersten Zeile extrahieren
+                    label = int(lines[0].split()[0])
                     dest_folder = ""
-                    for i in range(0,len(categories)):
+                    for i in range(0, len(categories)):
                         if label == i:
                             dest_folder = categories[i]
 
                     # Zielpfad für die Verschiebung erstellen
 
                     # Verschiebung der Datei in den entsprechenden Ordner
-                    #os.rename(txt_filepath, dest_path_txt)
-                    
-                    #dest_path_tmp = os.path.join(dest_dir, dest_folder, filename_txt)
-                    tmp = txt_filepath.replace(".txt",".jpg")
+                    # os.rename(txt_filepath, dest_path_txt)
+
+                    # dest_path_tmp = os.path.join(dest_dir, dest_folder, filename_txt)
+                    tmp = txt_filepath.replace(".txt", ".jpg")
                     jpg_filepath = tmp.replace("labels", "images")
 
-                    dest_path_jpg = os.path.join(dest_dir, dest_folder, filename_txt.replace(".txt",".jpg"))
+                    dest_path_jpg = os.path.join(
+                        dest_dir, dest_folder, filename_txt.replace(".txt", ".jpg"))
                     os.rename(jpg_filepath, dest_path_jpg)
 
     for filename in os.listdir(src_dir):
         p = os.path.join(src_dir, filename)
         if filename.endswith(".yaml"):
-            os.rename(p, dest_path_txt + "/" + filename)        
+            os.rename(p, dest_path_txt + "/" + filename)
     # TODO clean up directory
 
+
 if __name__ == "__main__":
-#    convert_dataset(["green", "red", "yellow"],
-#                    "/home/jay/module/ai_app/self_driving_cars/traffic_lights",
-#                    "/home/jay/module/ai_app/self_driving_cars/aai-selfdriving-cars/dataset/traffic_light" )
+    #    convert_dataset(["green", "red", "yellow"],
+    #                    "/home/jay/module/ai_app/self_driving_cars/traffic_lights",
+    #                    "/home/jay/module/ai_app/self_driving_cars/aai-selfdriving-cars/dataset/traffic_light" )
 
-    #p, i =  get_images('/home/jay/module/ai_app/self_driving_cars/aai-selfdriving-cars/dataset/traffic_light/original_data/green/')
+    # p, i =  get_images('/home/jay/module/ai_app/self_driving_cars/aai-selfdriving-cars/dataset/traffic_light/original_data/green/')
     pass
-
-
